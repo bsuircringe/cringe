5543dd1d5476a887978ad448c56b1b3383e2d42e
bsuircringe <95312566+bsuircringe@users.noreply.github.com>
2021-12-07 18:34:03 +0300

    811 block

---
+++ qwerty.js
@@ -0,0 +1,49 @@
+  function nik() {
+    document.location.href = 'nikita.html'
+  };
+  function art() {
+    document.location.href = 'artem.html'
+  };
+  function ego() {
+    document.location.href = 'egor.html'
+  };
+  function kir() {
+    document.location.href = 'kirill.html'
+  };
+  function gallery() {
+    document.location.href = 'gallery.html'
+  };
+  function grafic() {
+    document.location.href = 'grafic.html'
+  };
+  function index() {
+    document.location.href = 'index.html'
+  };
+  function artem_inst(){
+    document.location.href ='https://www.instagram.com/anoshkoartiom/'
+  };
+  function artem_vk(){
+    document.location.href = 'https://vk.com/anoshko4'
+  }
+  function nikita_vk(){
+    document.location.href = 'https://vk.com/rnikita2003'
+  }
+  function nikita_inst(){
+    document.location.href = 'https://www.instagram.com/nrublevskii/'
+  }
+  function egor_vk(){
+    document.location.href = 'https://vk.com/egor.kuch'
+  }
+  function egor_inst(){
+    document.location.href = 'https://www.instagram.com/kuchegorr/'
+  }
+  function kirill_vk(){
+    document.location.href = 'https://vk.com/likvl'
+  }
+  function kirill_inst(){
+    document.location.href = 'https://www.instagram.com/lifanovkirill_/'
+  }
+  function porn(){
+    document.location.href = 'https://www.pornhub.com'
+  }
+  
